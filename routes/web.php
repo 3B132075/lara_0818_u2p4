4e64416f622eb03aba19134c95592699a6ed5824
王翊安 <aud986587@gmail.com>
2024-12-07 16:47:52 +0800

    修正錯誤

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   return redirect(route('posts.index'));
 });
 
 Route::get('posts',[PostController::class, 'index'])->name('posts.index');
